027a03ac4633cfdf84d3a16be2599551beddcb07
AbdulSaboor100 <70238888+AbdulSaboor100@users.noreply.github.com>
2022-08-25 04:52:09 +0500

    change the url

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "https://clumsy-tunic-newt.cyclic.app";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://shab-store.herokuapp.com";
+// export const BASE_URL = "http://localhost:8000";
